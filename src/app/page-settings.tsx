e86be8305b1589e23ba582bf505020777271e16e
user <alejandro79861@gmail.com>
2024-09-11 12:26:21 -0500

    falta terminar

--- src/app/page-settings.tsx
+++ src/app/page-settings.tsx
@@ -3,8 +3,7 @@ import { ScrollView, Text } from 'react-native'
 export default function Settings() {
     return (
         <ScrollView className='flex-1 bg-bg-1 px-3'>
-            <Text className='text-white text-3xl'>Settings</Text>
-            <Text className='text-white text-xl'>Holaaaaaaaaaaaaaaaaaa</Text>
+            <Text className='text-white text-xl'>Invalid Data</Text>
         </ScrollView>
     )
 }
